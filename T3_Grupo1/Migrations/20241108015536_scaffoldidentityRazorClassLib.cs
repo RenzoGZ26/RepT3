ca9e2a8896b7c916784d15cd99c664042dc33e20
MarianaDG20 <n00310355@upn.pe>
2024-11-08 17:03:48 -0500

    Crud de la P1 terminada

---
+++ T3_Grupo1/Migrations/20241108015536_scaffoldidentityRazorClassLib.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace T3_Grupo1.Migrations
+{
+    public partial class scaffoldidentityRazorClassLib : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
